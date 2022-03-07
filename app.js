aa159160ece51e2755aea247a1abf67198bc6834
RS-Rak <rulingshadows@gmail.com>
2022-03-07 22:26:59 +0000

    BIG! Overhaul of web design

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// 
+// FAQ SECTION
+const accordion = document.getElementsByClassName("accordian")
+
+for (i=0; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        console.log("Click!")
+        this.classList.toggle('active')
+    })
+}
